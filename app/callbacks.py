7bd00854f8012a5891b9b7a4edd47130693ac242
imad-atraoui <imad.atraoui@gmail.com>
2023-03-24 12:32:51 +0000

    Application Web_Version_1

--- app/callbacks.py
+++ app/callbacks.py
@@ -1,7 +1,54 @@
 from dash import Input, Output, callback
 from France_Data import *
+from layouts import *
 
 input_list = ['PIB en fonction de l\'intensité de confinement', 'Evolution du PIB et de l\'indice d\'intensité de confinement', 'Evolution de nombre de vols passagers et de l\'intensité de confinement', 'Intensité de confinement et PIB en fonction du nombre total des cas']
+
+@callback(
+    Output("page-1-tab-content", "figure"),
+    Output('page-1-tab-value', "children"),
+    Input("page-1-tabs", "active_tab")
+)
+def display_value(tabs):
+    if tabs == 'Graphe 1' :
+        return (fig_2, "Analyse Graphe 1")
+    elif tabs == 'Graphe 2' :
+        return (fig_5, "Analyse Graphe 2")
+    elif tabs == 'Graphe 3' :
+        return (fig_6, "Analyse Graphe 3")
+    elif tabs == 'Graphe 4' :
+        return (fig_7, "Analyse Graphe 4")
+    #return f'You have selected {value}'
+
+@callback(
+    Output("page-2-tab-content", "figure"),
+    Output('page-2-tab-value', "children"),
+    Input("page-2-tabs", "active_tab")
+)
+def display_value(tabs):
+    if tabs == 'Graphe 1' :
+        return (fig_2, "Analyse Graphe 1")
+    elif tabs == 'Graphe 2' :
+        return (fig_5, "Analyse Graphe 2")
+    elif tabs == 'Graphe 3' :
+        return (fig_6, "Analyse Graphe 3")
+    elif tabs == 'Graphe 4' :
+        return (fig_7, "Analyse Graphe 4")
+
+@callback(
+    Output("page-3-tab-content", "figure"),
+    Output('page-3-tab-value', "children"),
+    Input("page-3-tabs", "active_tab")
+)
+def display_value(tabs):
+    if tabs == 'Graphe 1' :
+        return (fig_2, "Analyse Graphe 1")
+    elif tabs == 'Graphe 2' :
+        return (fig_5, "Analyse Graphe 2")
+    elif tabs == 'Graphe 3' :
+        return (fig_6, "Analyse Graphe 3")
+    elif tabs == 'Graphe 4' :
+        return (fig_7, "Analyse Graphe 4")
 
 @callback(
     Output('page-1-display-value', 'children'),
@@ -25,3 +72,10 @@ def display_value(value):
     Input('page-2-dropdown', 'value'))
 def display_value(value):
     return f'You have selected {value}'
+
+@callback(
+    Output('page-3-display-value', 'children'),
+    Input('page-3-dropdown', 'value'))
+def display_value(value):
+    return f'You have selected {value}'
+
